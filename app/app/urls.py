43035906970c9d286d49b38d928c44024b292255
“Gjorgi <georgiedelev@gmail.com>
2023-04-25 15:52:38 +0200

    startapp user

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from drf_spectacular.viewvs import (
+from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
@@ -27,7 +27,7 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path(
     'api/docs',
-    SpectacularSwaggerView(url_name='api-schema')
+    SpectacularSwaggerView.as_view(url_name='api-schema'),
     name='api-docs'
-    )
+    ),
 ]
