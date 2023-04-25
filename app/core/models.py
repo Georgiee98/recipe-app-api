65b1465712685a0c0fb2ea2c1d4cb915bcf857fd
“Gjorgi <georgiedelev@gmail.com>
2023-04-25 10:15:58 +0200

    Creating User Model and User Manager Models

--- app/core/models.py
+++ app/core/models.py
@@ -8,6 +8,18 @@ from django.contrib.auth.models import (
     BaseUserManager,
     PermissionsMixin,
 )
+
+
+class UserManager(BaseUserManager):
+    """Manager of users."""
+    def create_user(self, email, password=None, **extra_field):
+        """Create, save and returna  new user."""
+        user = self.model(email=email, **extra_field)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
 # AbstUsr.. Authentication features.... and PermMixin Permissions Features
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
