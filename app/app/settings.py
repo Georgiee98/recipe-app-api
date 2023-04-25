e18fd1fafc136adbbfb6098337d5d89def8d8e0b
“Gjorgi <georgiedelev@gmail.com>
2023-04-25 09:54:46 +0200

    deleting core/test.py.. apperantly caused a bug in ImportErrors...

--- app/app/settings.py
+++ app/app/settings.py
@@ -129,3 +129,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+TEST_DISCOVER_PATTERN = 'tests_models.py'
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,5 @@
 """Tests for all kind of models"""
-from django.test import TestCase
+from core import tests
 from django.contrib.auth import get_user_model
 
 
@@ -8,12 +8,11 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful"""
-
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
-            email = email
-            password = password
+            email = email,
+            password = password,
         )
 
         self.assertEqual(user.email, email)
