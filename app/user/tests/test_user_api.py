cb289e591dde3975c6a2660a8e6d1e8ae827dfaa
“Gjorgi <georgiedelev@gmail.com>
2023-04-25 16:07:13 +0200

    Test the public features of the user APID

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
-from rest_framework.test import APIClinet
+from rest_framework.test import APIClient
 from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
@@ -14,3 +14,25 @@ def create_user(**params):
     """Create and return a new user."""
     return get_user_model().objects.create_user(**params)
 
+
+# Public Tests - Unauthenticated requests
+class PublicUserApiTest(TestCase):
+    """Test the public features of the user API"""
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_user_success(self):
+        """Test creating a user is successful."""
+        payload = {
+            'email' : 'test@example.com',
+            'password': 'testpass123',
+            'name': 'Test Name',
+        }
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(email=payload['email'])
+        self.assertTrue(user.check_password(payload['password']))
+        # self.assertTrue(user.check_password(payload['name']))
+        self.assertNotIn('password', res.date) # don't return the password as a response
