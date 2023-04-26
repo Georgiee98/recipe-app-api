bcf8bf8ac476ab501c1a39ada8fe593c4d569fc1
“Gjorgi <georgiedelev@gmail.com>
2023-04-26 17:56:46 +0200

    Serializer for the user auth token

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,7 +1,11 @@
 """
 Serializers for the user API View.
 """
-from django.contrib.auth import get_user_model
+from django.contrib.auth import (
+    get_user_model,
+    authenticate,
+)
+from django.utils.translation import gettext as _
 
 from rest_framework import serializers
 
@@ -16,3 +20,14 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user auth token."""
+    email = serializer.EmailField()
+    password = serializer.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace = False
+    )
+
+
