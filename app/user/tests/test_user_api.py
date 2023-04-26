f42b2a90ec87ec03cac0517391618db3253fb3a7
“Gjorgi <georgiedelev@gmail.com>
2023-04-26 18:33:30 +0200

    Test POST is not allowed for the (me) endpoint

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -136,4 +136,8 @@ class PrivateUserApiTest(TestCase):
             'email': self.user.email,
         })
 
+    def test_post_me_not_allowed(self):
+        """Test POST is not allowed for the me endpoint."""
+        res = self.client.post(ME_URL, {})
 
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
