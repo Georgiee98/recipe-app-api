3ac949b4f072c2207b401f017cf92c6681cfe852
“Gjorgi <georgiedelev@gmail.com>
2023-04-26 18:23:23 +0200

    View and URL for Creating a new auth token for user.

--- app/user/views.py
+++ app/user/views.py
@@ -8,7 +8,7 @@ from rest_framework.settings import api_settings
 
 from user.serializers import (
     UserSerializer,
-
+    AuthTokenSerializer,
 )
 
 class CreateUserView(generics.CreateAPIView):
@@ -16,3 +16,7 @@ class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
 
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user."""
+    serializer_classs = AuthTokenSerializer
+    rendere_classes = api_settings.DEFAULT_RENDERER_CLASSES
