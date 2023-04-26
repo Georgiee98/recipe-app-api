bd7db9395f5fbbdcd127a3a89d25579088069fb2
“Gjorgi <georgiedelev@gmail.com>
2023-04-26 18:30:51 +0200

    Test retrieveing profile for logged in user.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -126,5 +126,14 @@ class PrivateUserApiTest(TestCase):
         self.client.force_authenticate(user=self.user)
 
 
+    def test_retrieve_profile_success(self):
+        """Test retrieveing profile for logged in user."""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'name': self.user.name,
+            'email': self.user.email,
+        })
 
 
