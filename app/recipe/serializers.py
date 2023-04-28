e24b05c4fe8549e641801c7639949a4e10aededa
“Gjorgi <georgiedelev@gmail.com>
2023-04-28 14:33:34 +0200

    Serializer for tag

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -4,7 +4,19 @@ from rest_framework import serializers
 
 from core.models import (
     Recipe,
+    Tag,
+
 )
+
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tags."""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
 
 
 
