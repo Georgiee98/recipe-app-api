82e62c2be46f0c6ef9a4913d74010f9df1892122
“Gjorgi <georgiedelev@gmail.com>
2023-04-28 16:25:24 +0200

    Test Create recipe

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -46,3 +46,20 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+
+    def test_create_recipe(self):
+    """Test creating a recipe is successful."""
+    user = get_user_model().objects.create_user(
+        'test@example.com',
+        'testpass123',
+    )
+    recipe = models.Recipe.objects.create(
+        user=user,
+        title='Sample recipe name',
+        time_minutes=5,
+        price=Decimal('5.50'),
+        description='Sample receipe description.',
+    )
+
+    self.assertEqual(str(recipe), recipe.title)
