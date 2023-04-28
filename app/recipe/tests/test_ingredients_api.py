de7cb8e329b7601d3c25a776569e9f3f28f7e987
“Gjorgi <georgiedelev@gmail.com>
2023-04-28 16:33:02 +0200

    Test unauthenticated API requests

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -27,3 +27,17 @@ def detail_url(ingredient_id):
 def create_user(email='user@example.com', password='testpass123'):
     """Create and return user."""
     return get_user_model().objects.create_user(email=email, password=password)
+
+
+
+class PublicIngredientsApiTests(TestCase):
+    """Test unauthenticated API requests."""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        """Test auth is required for retrieving ingredients."""
+        res = self.client.get(INGREDIENTS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
