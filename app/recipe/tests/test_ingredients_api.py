cd64adb487530d4b30193dc74d6a6ace3b221f50
“Gjorgi <georgiedelev@gmail.com>
2023-04-28 16:31:47 +0200

    test_ingredients_api imports

---
+++ app/recipe/tests/test_ingredients_api.py
@@ -0,0 +1,20 @@
+"""
+Tests for the ingredients API.
+"""
+from decimal import Decimal
+
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import (
+    Ingredient,
+    Recipe,
+)
+
+from recipe.serializers import IngredientSerializer
+
+INGREDIENTS_URL = reverse('recipe:ingredient-list')
